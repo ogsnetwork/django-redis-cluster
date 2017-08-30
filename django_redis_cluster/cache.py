38646574f90c4c730c5aa2a4c64b7e9186a3b2f7
Seiyifa Tawari <seiyifa.tawari@gmail.com>
2017-08-30 08:11:14 -0700

    chore(cache): remove all logging

--- django_redis_cluster/cache.py
+++ django_redis_cluster/cache.py
@@ -19,41 +19,38 @@ if DJANGO_REDIS_CLUSTER_IGNORE_EXCEPTIONS:
 
 def omit_exception(method=None, return_value=None):
     '''连接异常处理修饰器'''
-    
+
     if method is None:
         return functools.partial(omit_exception, return_value=return_value)
-    
+
     @functools.wraps(method)
     def _decorator(self, *args, **kwargs):
         try:
             return method(self, *args, **kwargs)
         except ConnectionInterrupted as e:
             if self._ignore_exceptions:
-                if DJANGO_REDIS_CLUSTER_LOG_IGNORED_EXCEPTIONS:
-                    logger.error(str(e))
-                    
                 return return_value
             raise e.parent
     return _decorator
-   
+
 
 class RedisClusterCache(BaseCache):
-    
+
     def __init__(self, server, params):
-        
+
         super(RedisClusterCache, self).__init__(params)
 
         self._server = server
         self._params = params
-        
+
         options = params.get('OPTIONS', {})
         self._client_cls = options.get("CLIENT_CLASS", "django_redis_cluster.client.DefaultClient")
         self._client_cls = load_class(self._client_cls)
         self._client = None
-        
-        self._ignore_exceptions = options.get('IGNORE_EXCEPTIONS', DJANGO_REDIS_CLUSTER_IGNORE_EXCEPTIONS)
-        
-    
+
+        self._ignore_exceptions = options.get('IGNORE_EXCEPTIONS', True)
+
+
     @property
     def client(self):
         '''
@@ -62,99 +59,97 @@ class RedisClusterCache(BaseCache):
         if self._client is None:
             self._client = self._client_cls(self._server, self._params, self)
         return self._client
-    
-    
+
+
     @omit_exception
     def add(self, *args, **kwargs):
         return self.client.add(*args, **kwargs)
-    
-    
+
+
     @omit_exception
     def set(self, *args, **kwargs):
         return self.client.set(*args, **kwargs)
-    
-    
+
+
     @omit_exception
     def set_many(self, *args, **kwargs):
         return self.client.set_many(*args, **kwargs)
-    
-    
+
+
     @omit_exception
     def get(self, key, default=None, version=None):
         try:
             return self.client.get(key, default=default, version=version)
         except ConnectionInterrupted as e:
-            if DJANGO_REDIS_CLUSTER_LOG_IGNORED_EXCEPTIONS:
-                logger.error(str(e))
             return default
         raise
-    
-    
+
+
     @omit_exception(return_value={})
     def get_many(self, *args, **kwargs):
         return self.client.get_many(*args, **kwargs)
-    
-    
+
+
     @omit_exception
     def delete(self, *args, **kwargs):
         return self.client.delete(*args, **kwargs)
-    
-    
+
+
     @omit_exception
     def delete_pattern(self, *args, **kwargs):
         return self.client.delete_pattern(*args, **kwargs)
-    
-    
+
+
     @omit_exception
     def delete_many(self, *args, **kwargs):
         return self.client.delete_many(*args, **kwargs)
 
-    
+
     @omit_exception
     def clear(self):
         return self.client.clear()
-    
-        
+
+
     @omit_exception
     def keys(self, *args, **kwargs):
         return self.client.keys(*args, **kwargs)
-    
-    
+
+
     @omit_exception
     def has_key(self, *args, **kwargs):
         return self.client.has_key(*args, **kwargs)
-    
-    
+
+
     @omit_exception
     def lock(self, *args, **kwargs):
         return self.client.lock(*args, **kwargs)
 
-    
+
     @omit_exception
     def incr_version(self, *args, **kwargs):
         return self.client.incr_version(*args, **kwargs)
-    
-    
+
+
     @omit_exception
     def incr(self, *args, **kwargs):
         return self.client.incr(*args, **kwargs)
-    
-    
+
+
     @omit_exception
     def decr(self, *args, **kwargs):
         return self.client.decr(*args, **kwargs)
-    
-    
+
+
     @omit_exception
     def iter_keys(self, *args, **kwargs):
         return self.client.iter_keys(*args, **kwargs)
-    
-    
+
+
     @omit_exception
     def ttl(self, *args, **kwargs):
         return self.client.ttl(*args, **kwargs)
-    
-    
+
+
     @omit_exception
     def persist(self, *args, **kwargs):
         return self.client.persist(*args, **kwargs)
@@ -162,4 +157,4 @@ class RedisClusterCache(BaseCache):
     @omit_exception
     def expire(self, *args, **kwargs):
         return self.client.expire(*args, **kwargs)
-    
+
